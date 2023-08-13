1f632cff0af6bad2d97ba266aa6ff63d555670c0
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2023-08-14 01:18:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import requests
 
 # Dictionary of actors and their corresponding video URLs
 ACTOR_VIDEOS = {
-    "Original": "opp.mp4",
-    "Actor 1": "download.mp4",
-    "Actor 2": "download.mp4",
+    "Original": "https://github.com/AI-ANK/bmjoanisawful/blob/main/opp.mp4",
+    "Actor 1": "https://github.com/AI-ANK/bmjoanisawful/blob/main/download.mp4",
+    "Actor 2": "https://github.com/AI-ANK/bmjoanisawful/blob/main/download.mp4",
     # Add more actors and their corresponding videos
 }
 
