f0f98c0bb6afbf03cbc4f039554917e1aaf5b179
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2023-08-16 23:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,29 +2,43 @@ import streamlit as st
 import requests
 from streamlit_image_select import image_select
 
-# Dictionary of actors, their corresponding video URLs, and image URLs
-ACTOR_VIDEOS = {
-    "Original": {
-        "video_url": "https://cdn.jsdelivr.net/gh/AI-ANK/bmjoanisawful@main/opp.mp4",
-        "image_url": "https://github.com/AI-ANK/bmjoanisawful/raw/cf49344156010790d3a3ca56f66c1ee5de7060a4/images/jimmy.jpg",
-    },
-    "Actor 1": {
-        "video_url": "https://cdn.jsdelivr.net/gh/AI-ANK/bmjoanisawful@main/download.mp4",
-        "image_url": "https://github.com/AI-ANK/bmjoanisawful/raw/cf49344156010790d3a3ca56f66c1ee5de7060a4/images/john.jpg",
-    },
-    "Actor 2": {
-        "video_url": "https://cdn.jsdelivr.net/gh/AI-ANK/bmjoanisawful@main/download.mp4",
-        "image_url": "https://github.com/AI-ANK/bmjoanisawful/raw/cf49344156010790d3a3ca56f66c1ee5de7060a4/images/joaquin.jpg",
-    },
-    # Add more actors and their corresponding videos and images
-}
+REPO_OWNER = "AI-ANK"
+REPO_NAME = "bmjoanisawful"
+VIDEO_FOLDER_PATH = "videos"
+IMAGE_FOLDER_PATH = "images"
+DEFAULT_IMAGE_URL = "url_of_default_image.jpg"  # replace with your default image URL
+
+# Fetch files from GitHub repo's 'videos' folder
+video_files_response = requests.get(f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{VIDEO_FOLDER_PATH}")
+video_files = [file['name'] for file in video_files_response.json()]
+
+# Initialize actor_videos dictionary
+ACTOR_VIDEOS = {}
+
+# Populate ACTOR_VIDEOS with video URLs and corresponding image URLs
+for video_file in video_files:
+    actor_name = video_file.split('.')[0]  # assuming file names without dots except for the extension
+    video_url = f"https://github.com/{REPO_OWNER}/{REPO_NAME}/raw/main/{VIDEO_FOLDER_PATH}/{video_file}"
+    
+    # Check if corresponding image exists
+    image_file = f"{actor_name}.jpg"  # assuming image format is jpg
+    image_url = f"https://github.com/{REPO_OWNER}/{REPO_NAME}/raw/main/{IMAGE_FOLDER_PATH}/{image_file}"
+    response = requests.head(image_url)
+    if response.status_code != 200:
+        image_url = DEFAULT_IMAGE_URL
+    
+    ACTOR_VIDEOS[actor_name] = {
+        "video_url": video_url,
+        "image_url": image_url,
+    }
 
 # Legal Disclaimer Button
 if st.button('View Legal Disclaimer'):
     st.warning("""
-    **Legal Disclaimer**
-    This demonstration is intended solely for...
-    ... [continue the rest of the disclaimer here]
+     **Legal Disclaimer**
+    This demonstration is intended solely for educational and learning purposes. The videos, images, and other content used in this demonstration are the intellectual property of their respective owners. This demonstration does not claim any ownership over these materials and is not intended for commercial use.
+    The use of copyrighted material in this demonstration is believed to fall under the "fair use" doctrine, as it is used for educational purposes, is non-commercial in nature, and does not materially affect the potential market for or value of the copyrighted work. Nevertheless, if you are a copyright holder and believe that any content used in this demonstration infringes upon your rights, please contact us at [your contact information here], and we will promptly address your concerns.
+    By accessing this demonstration, you acknowledge and agree that you understand the nature of this demonstration and that you will not use it for any unlawful or prohibited purposes. The creators of this demonstration make no representations or warranties regarding the accuracy, legality, or completeness of the content and disclaim all liability for any damages arising from the use of this demonstration.
     """)
 
 # Title of the app
