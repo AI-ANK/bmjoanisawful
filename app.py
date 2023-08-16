3eda08fb525d1e66cd523dea16da41a4dd34e942
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2023-08-16 23:47:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,10 @@ ACTOR_VIDEOS = {
 # Legal Disclaimer Button
 if st.button('View Legal Disclaimer'):
     st.warning("""
-    **Legal Disclaimer**
-    This demonstration is intended solely for...
-    ... [continue the rest of the disclaimer here]
+     **Legal Disclaimer**
+    This demonstration is intended solely for educational and learning purposes. The videos, images, and other content used in this demonstration are the intellectual property of their respective owners. This demonstration does not claim any ownership over these materials and is not intended for commercial use.
+    The use of copyrighted material in this demonstration is believed to fall under the "fair use" doctrine, as it is used for educational purposes, is non-commercial in nature, and does not materially affect the potential market for or value of the copyrighted work. Nevertheless, if you are a copyright holder and believe that any content used in this demonstration infringes upon your rights, please contact us at [your contact information here], and we will promptly address your concerns.
+    By accessing this demonstration, you acknowledge and agree that you understand the nature of this demonstration and that you will not use it for any unlawful or prohibited purposes. The creators of this demonstration make no representations or warranties regarding the accuracy, legality, or completeness of the content and disclaim all liability for any damages arising from the use of this demonstration.
     """)
 
 # Title of the app
