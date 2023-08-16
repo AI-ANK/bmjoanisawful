e9984066983127685c8d3c823631c88e2633b194
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2023-08-17 02:32:26 +0530

    Changed layout with video on top and image on bottom

--- app.py
+++ app.py
@@ -35,32 +35,34 @@ if st.button('View Legal Disclaimer'):
     By accessing this demonstration, you acknowledge and agree that you understand the nature of this demonstration and that you will not use it for any unlawful or prohibited purposes. The creators of this demonstration make no representations or warranties regarding the accuracy, legality, or completeness of the content and disclaim all liability for any damages arising from the use of this demonstration.
     """)
 
-# Title of the app
-#st.title("Choose Your Actor")
-st.title("Black Mirror Meets The Office: Michael Scott Is Awful")
-st.markdown("Select an actor's face below and watch them transform into Michael Scott")
+# If session state doesn't have the selected_index yet, initialize it to 0 (default)
+if 'selected_index' not in st.session_state:
+    st.session_state['selected_index'] = 0
 
+# Use the current session state's selected_index to get the video URL
+selected_video_url = ACTOR_VIDEOS[actor_names[st.session_state['selected_index']]]["video_url"]
+
+# Check if the video URL is valid and display it
+response = requests.head(selected_video_url)
+if response.status_code == 200:
+    st.markdown(f'<video width="100%" controls autoplay src="{selected_video_url}"></video>', unsafe_allow_html=True)
+else:
+    st.error("Video not found. Please check the URL.")
 
 # Display actor images for selection
 actor_names = list(ACTOR_VIDEOS.keys())
 actor_images = [ACTOR_VIDEOS[actor]['image_url'] for actor in actor_names]
 
-selected_index = image_select(
+# Use image_select to get the index of the clicked actor's image
+clicked_index = image_select(
     "",
     images=actor_images,
     captions=actor_names,
-    index=0,
+    index=st.session_state['selected_index'],  # Use the session state's selected index as the default
     return_value="index",
     use_container_width=0,
 )
 
-# Use the selected index to get the video URL
-selected_video_url = ACTOR_VIDEOS[actor_names[selected_index]]["video_url"]
-
-# Check if the video URL is valid
-response = requests.head(selected_video_url)
-if response.status_code == 200:
-    #st.video(selected_video_url)
-    st.markdown(f'<video width="100%" controls autoplay src="{selected_video_url}"></video>', unsafe_allow_html=True)
-else:
-    st.error("Video not found. Please check the URL.")
+# If the clicked index is different from the current selected_index, update the session state
+if clicked_index != st.session_state['selected_index']:
+    st.session_state['selected_index'] = clicked_index
